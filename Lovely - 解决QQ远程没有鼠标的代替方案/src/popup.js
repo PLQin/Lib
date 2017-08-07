27909aef50014488f66b3db575345e25528bbb43
qingguipeng <773561801@qq.com>
2017-08-07 09:21:52 +0800

    * 插件与浏览器DOM的交互

--- Lovely - 解决QQ远程没有鼠标的代替方案/src/popup.js
+++ Lovely - 解决QQ远程没有鼠标的代替方案/src/popup.js
@@ -1,49 +1,57 @@
 
 /**
- * 本地从 扩展中调用JS
+ *	本地从 扩展中调用JS
  * 		chrome.extension.getURL("Lovely.js")
  *
+ *  ripple 插件（按钮点击水波纹插件）的调用方法：
+ *	 	Array.prototype.forEach.call(document.querySelectorAll('[data-ripple]'), function(element){
+ *	  		// element is instance of javascript element node
+ *	    	// element 是DOM元素实例
+ *	      	new RippleEffect(element); 
+ *	    });
  * 
  */
 
-
- 
-
-
-
-// function fnClick() {
-// 	alert(1)
-// }
 
 
 // // chrome.browserAction.onClicked.addListener(fnClick);
 // fnClick()
 
-// // 设置一个点击browser actions时显示在popup中的HTML。
+// var port = chrome.extension.connect();
+
+// 设置一个点击browser actions时显示在popup中的HTML。
 // chrome.browserAction.setPopup(fnClick)
-// 
-// 
-        // find all elements and attach effect
-        // 找到所有的元素使其附加属性
-	 //    Array.prototype.forEach.call(document.querySelectorAll('[data-ripple]'), function(element){
-	 //    	// element is instance of javascript element node
-	 //    	// element 是DOM元素实例
-	 //        new RippleEffect(element); 
-	 //    });
+		
+// find all elements and attach effect
+// 找到所有的元素使其附加属性
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 	// 之前没有这行代码， 报错： 没有权限
+	 	// 引用JS后， 可以知晓 Lovely 下的方法
+	 	// permissions要设置好
+		chrome.extension.getURL("Lovely.js")
+
 
 		document.getElementById("start-btn").addEventListener("click",  function (event) {
+			// 可以传送
+			// http://open.chrome.360.cn/extension_dev/content_scripts.html
 			chrome.tabs.executeScript(null,{code:"Lovely.init()"});
 		})
 
 		document.getElementById("stop-btn").addEventListener("click",  function (event) {
 			chrome.tabs.executeScript(null,{code:"Lovely.fnHide()"});
 		})
-
-
-		// var port = chrome.extension.connect();
-
-		// // 设置一个点击browser actions时显示在popup中的HTML。
-		// chrome.browserAction.setPopup(fnClick)
 
 
 
